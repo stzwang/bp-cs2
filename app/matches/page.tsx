b9866d3627c9a480326aaf5e90c9e9084b4339ac
Steve <stzwang@gmail.com>
2026-06-17 14:37:27 -0700

    fix: replace caret char with exact Tabler IconCaretLeftFilled SVG path at size 12

--- app/matches/page.tsx
+++ app/matches/page.tsx
@@ -202,19 +202,30 @@ function TeamRow({
           style={{
             fontWeight: won ? 900 : 300,
             fontSize: 14,
-            minWidth: 14,
-            textAlign: "right",
+            width: 12,
+            textAlign: "center",
           }}
         >
           {score}
         </span>
-        {won ? (
-          <span style={{ fontSize: 9, color: "var(--muted)", marginLeft: 4, lineHeight: 1 }}>◄</span>
-        ) : (
-          <div style={{ marginLeft: 4, width: 9 }} />
-        )}
+        {won ? <CaretLeft /> : <div style={{ marginLeft: 4, width: 12 }} />}
       </div>
     </div>
+  );
+}
+
+function CaretLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="12"
+      height="12"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      style={{ marginLeft: 4, flexShrink: 0 }}
+    >
+      <path d="M13.883 5.007l.058 -.005h.118l.058 .005l.06 .009l.052 .01l.108 .032l.067 .027l.132 .07l.09 .065l.081 .073l.083 .094l.054 .077l.054 .096l.017 .036l.027 .067l.032 .108l.01 .053l.01 .06l.004 .057l.002 .059v12c0 .852 -.986 1.297 -1.623 .783l-.084 -.076l-6 -6a1 1 0 0 1 -.083 -1.32l.083 -.094l6 -6l.094 -.083l.077 -.054l.096 -.054l.036 -.017l.067 -.027l.108 -.032l.053 -.01l.06 -.01z" />
+    </svg>
   );
 }
 
